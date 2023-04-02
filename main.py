e121239295b5d7eff21495295d86c4e5059ae456
Viacheslav Sukhenko <slavichmy@gmail.com>
2023-04-02 14:56:50 +0300

    add fuctionality

---
+++ main.py
@@ -0,0 +1,31 @@
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '5590048325:AAFi-S4eIlzUj47ER8MYDYGUehC-WzC-n2k'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hi!\nI'm Freiherr bot!\nPowered by Sukhenko.")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+	if message.text == 'Video':
+		await bot.send_video(message.chat.id, open('videoplayback.mp4', 'rb'))
+	elif message.text == 'Music':
+		await bot.send_audio(message.chat.id, open('music1.mp3', 'rb'))
+	else:
+		await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
